b668d943c38dc9a62dbe54413726c0a76c3e4467
vudangkhoa1993 <dangkhoavu1993@gmail.com>
2021-05-06 23:07:17 +0700

    update CRUD

--- index.js
+++ index.js
@@ -3,73 +3,77 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const ToDoTask = require('./models/todotask');
-const todotask = require('./models/todotask');
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
 
 dotenv.config();
-
 // create virtual path prefix 
 app.use('/static', express.static('public'));
-
 // extract data from body request
-app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false })); // question for this one
 
 // made a connection to DB
 mongoose.set('useFindAndModify', false);
+
 mongoose.connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, () => {
     console.log('Connected to DB successfully!');
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
         console.log('server up and running on port 3000');
     });
 });
 
-
-// setting view engine configruration 
-app.set('view engine', 'ejs');
-
 // GET method
-app.get('/',async (req, res, next) => {
-    const tasks = await ToDoTask.find({});
-    res.json(tasks);
-    // ToDoTask.find({}, (error, tasks) => {
-    //     res.render('todo.ejs', { todoTasks: tasks });
-    //     // res.json(tasks);
-    // });
-});
-
-// POST method
-app.post('/', async (req, res, next) => {
-    const todoTask = new ToDoTask({
-        content: req.body.content
-    });
+app.get('/', async (req, res, next) => {
     try {
-        await todoTask.save();
-        res.status(201);
+        const tasks = await ToDoTask.find({});
+        res.json(tasks);
     }
     catch (err) {
         res.status(500, err);
     }
 });
 
+// POST method
+app.post('/', jsonParser, async (req, res, next) => {
+    try {
+        const todoTask = new ToDoTask({
+            content: req.body.content
+        });
+        await todoTask.save();
+        res.sendStatus(200);
+    }
+    catch (err) {
+        console.log(err);
+        next(err);
+        res.sendStatus(500, err);
+    }
+});
+
 // UPDATE
-app.put('/edit/:id',async (req, res) => {
+app.put('/edit/:id', jsonParser, async (req, res, next) => {
     const id = req.params.id;
     try {
-        await ToDoTask.findByIdAndUpdate(id, { content: req.body.content});
-        res.status(200);
+        await ToDoTask.findByIdAndUpdate(id, { content: req.body.content });
+        res.sendStatus(200);
     }
-    catch(error) {
-        res.status(500, error);
+    catch (error) {
+        next(error);
+        res.sendStatus(500, error);
     }
 });
 
 // DELETE
-app.delete('/delete/:id', async (req, res) => {
+app.delete('/delete/:id', async (req, res, next) => {
     const id = req.params.id;
-    await ToDoTask.findByIdAndRemove(id, (err) => {
-        if(err) return res.statusCode(500, err);
-        res.status(200);
-    })
+    try {
+        await ToDoTask.findByIdAndRemove(id);
+        res.sendStatus(200);
+    }
+    catch (error) {
+        next(error);
+        res.sendStatus(500, error);
+    }
 });
